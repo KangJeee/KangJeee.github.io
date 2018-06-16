143356bd25890879ba530e0c7b3d7887b567164b
KangJeee <39054232+KangJeee@users.noreply.github.com>
2018-06-16 21:32:43 +0900

    asd
    asd

--- sources/CALC.js
+++ sources/CALC.js
@@ -145,7 +145,7 @@ $(function() {
     });
 
     /*계산기 구현 영역*/
-    $buttons.touchstart(function (){
+    $buttons.mousedown(function (){
         var newVal = $(this).val();
         var $inval = $("#inval");
         var $oldval = $inval.val();
